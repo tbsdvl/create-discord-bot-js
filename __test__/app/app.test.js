d7e4407aa131d6e31a4d98f888391c39b4eb5033
unknown <tristonburns97@gmail.com>
2022-05-28 00:15:36 -0700

    Update DiscordBot constructor params to take an options object and a nullable middleware array

--- __test__/app/app.test.js
+++ __test__/app/app.test.js
@@ -3,53 +3,39 @@ import { startUpDiscordBot } from '../../main/src/utils/botUtils';
 import express from "express";
 import 'jest';
 
-// Setup an App interface that has fields for
-// express(), a port, an array of middleware, 
-// listens and executes some additional code
+let Bot;
 
-// App interface will include fields for
-// the express function, an integer for a port,
-// and an array of middleware functions
+beforeAll(() => {
+    // Setup test object for instances of App interface
+    const options = {
+        app: express(),
+        PORT: 3000,
+        // middleware: [express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) })]
+    }
 
-// App interface will also include a constructor including
-// parameters for all of its fields, and methods for 
-// executing express, middleware functions, and listening for ports
+    const newBot = new DiscordBot(options);
+    Bot = newBot;
+});
 
-// In the future, App will include a method for checking & installing
-// new commands on the discord server
+describe('DiscordBot constructor', () => {
 
-describe('App interface', () => {
-
-    let Bot;
-
-    beforeAll(() => {
-        // Setup test object for instances of App interface
-        const options = {
-            app: express,
-            PORT: 3000,
-            // middleware: [express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) })]
-        }
-
-        const newBot = new DiscordBot(options.app(), options.PORT);
-        Bot = newBot;
-    });
     
     it('should instantiate a new Bot object of type App', () => {
         expect(Bot).not.toBeNull();
     });
 
-    // Check for a bot's express app
-
-    // Check for a bot's port
     it("should return the type of the DiscordBot's express app", () => {
         expect(typeof Bot.app).toBe(typeof express);
     });
-
-    it("should return the PORT for the DiscordBot", () => {
+    
+    it("should return the PORT property for the DiscordBot instance", () => {
         expect(Bot.PORT).toBe(3000);
     });
+});
 
+describe('startUpDiscordBot', () => {
+    
     it('should start the app to listen on port 3000', () => {
         expect(startUpDiscordBot(Bot)).toBe(true);
     });
-})
+});
